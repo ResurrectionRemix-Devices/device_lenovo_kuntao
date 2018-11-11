0badc2966f90e7063f49d83435acfed4e74e5ddd
Murat Kozan <muratkozan350@gmail.com>
2018-11-11 18:54:22 +0300

    kuntao: Add new media codec policy
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- device.mk
+++ device.mk
@@ -342,7 +342,8 @@ PRODUCT_PACKAGES += \
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
+    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp_policy/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
 
 # Sensor
 PRODUCT_PACKAGES += \
