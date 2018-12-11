cca79c995f9d0bef0d02d86d2f504b037758685c
Murat Kozan <muratkozan350@gmail.com>
2018-12-11 17:26:06 +0300

    kuntao: Enable custom health hal

--- device.mk
+++ device.mk
@@ -200,7 +200,7 @@ PRODUCT_PACKAGES += \
 
 # Healthd
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-service \
+    android.hardware.health@2.0-service.kuntao
 
 # IMS
 PRODUCT_PACKAGES += \
