91ed19c00fcf884c812ccd378b783b616bc60669
Murat Kozan <muratkozan350@gmail.com>
2018-12-28 01:12:55 +0300

    kuntao: Enable ANT+ support

--- device.mk
+++ device.mk
@@ -84,6 +84,15 @@ PRODUCT_COPY_FILES += \
    $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
    $(LOCAL_PATH)/audio/mixer_paths_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_mtp.xml
 
+# ANT+
+PRODUCT_PACKAGES += \
+    AntHalService \
+    com.dsi.ant.antradio_library \
+    libantradio
+
+PRODUCT_COPY_FILES += \
+    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
+
 # Charger
 PRODUCT_PACKAGES += \
    charger_res_images
