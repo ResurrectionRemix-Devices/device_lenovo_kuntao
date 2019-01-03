5fe93d030699b19b9eab28d03200f1d4bfbc810b
Murat Kozan <muratkozan350@gmail.com>
2019-01-03 20:11:16 +0300

    kuntao: Remove bluetooth service
    *No need, Enabled bluetooth service qti

--- device.mk
+++ device.mk
@@ -1,4 +1,4 @@
-:#
+#
 # Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -96,10 +96,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
-
-# Bluetooth
-PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-service
 
 # Charger
 PRODUCT_PACKAGES += \
