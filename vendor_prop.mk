4763f502416edcfa7ad0c890aae8dc033823f4a1
abdou200315 <32501746+abdou200315@users.noreply.github.com>
2019-03-30 22:45:24 +0100

    Kuntao:  AudioFlinger client heap Fix

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -40,7 +40,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.audio.playback.mch.downsample=true \
     vendor.audio.safx.pbe.enabled=true \
     vendor.voice.path.for.pcm.voip=true \
-    ro.af.client_heap_size_kbyte=7168 \
     persist.audio.dirac.speaker=true \
     persist.vendor.audio.hw.binder.size_kbyte=1024
 
